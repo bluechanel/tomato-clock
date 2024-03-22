4a3673f80516f394668e45cd617b3106a24254c8
wiley <bluechanel612@gmail.com>
2024-03-22 10:27:41 +0800

    添加google分析

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,19 @@ export default function RootLayout({
 
   return (
     <html lang="en" className={theme ? "light" : "dark"}>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ER2VE60WFT"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ER2VE60WFT');`
+        }}>
+        </script>
+      </head>
       <body className="flex flex-col">
         <Navbar maxWidth="full">
           <NavbarContent justify="end">
