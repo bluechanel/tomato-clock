82fd3ea7f404ab54035db6ae577ffada3fe87dd4
wiley <bluechanel612@gmail.com>
2024-03-25 10:38:54 +0800

    fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 'use client'
+import { useEffect } from 'react';
 import Timer from './timer/page';
 
 export default function Page() {
-  if (window.Notification.permission != "denied") {
-    window.Notification.requestPermission();
-  }
+
+  useEffect(() => {
+    if (window.Notification.permission != "denied") {
+      window.Notification.requestPermission();
+    }
+  })
+
   return (
     <div className='h-full flex justify-center items-center'>
       <Timer />
