e1cc736559847cdb72731d207dfcd714a026e7d3
wiley <bluechanel612@gmail.com>
2024-05-17 15:21:46 +0800

    代码逻辑优化

--- src/app/timer/page.tsx
+++ src/app/timer/page.tsx
@@ -10,13 +10,21 @@ import CircleRingWithContent from "@/components/ring/ring";
 import { showNotification } from "@/utils/notification";
 
 
+// 计时器类型
+export enum TimerState {
+    Focusing = "focus",
+    Breaking = "break",
+    LongBreaking = "longBreak",
+    Stop = "stop"
+}
 
-function TimerButton({ state, startFunc, stopFunc, extendFunc }: { state: string, startFunc: () => void, stopFunc: () => void, extendFunc: () => void }) {
-    if (state == "") {
+
+function TimerButton({ state, startFunc, stopFunc, extendFunc }: { state: TimerState, startFunc: () => void, stopFunc: () => void, extendFunc: () => void }) {
+    if (state == TimerState.Stop) {
         return <Button color="primary" radius="full" className="w-1/4" onClick={startFunc}>
             Start
         </Button>
-    } else if (state == "focusing" || state == "breaking" || state == "Long breaking") {
+    } else if (state == TimerState.Focusing || state == TimerState.Breaking || state == TimerState.LongBreaking) {
         // return <><Button color="primary" radius="full" className="w-1/4" onClick={extendFunc}>
         //     Extend(5 min)
         // </Button><Button color="primary" radius="full" className="w-1/4" onClick={stopFunc}>
@@ -37,60 +45,44 @@ export default function Timer() {
 
     const initTimeProgress = { timeLeft: Number(setting.getItem("focus")) * 60 * 1000, percent: 1 }
 
-    const [state, setState] = useState<string>("");
+    const [state, setState] = useState<TimerState>(TimerState.Stop);
     const [value, setValue] = useState<TimeProgress>(initTimeProgress);
     const [timerId, setTimerId] = useState<number | undefined>(undefined);
-    const [focusList, updateFocusLsit] = useImmer<string[]>([]);
+    const [focusList, updateFocusLsit] = useImmer<TimerState[]>([]);
 
 
-    const startFocusTimer = () => {
-        const countdown = new Countdown((timeDiff) => {
-            setValue(timeDiff);
-        }, () => {
-            const tip = showNotification("Focus stopped");
-            tip.onclick = () => {
-                window.focus();
+    const selectTimerType = (): TimerState => {
+        const fs = focusList.slice(-5);
+        if (fs.at(-1) == TimerState.Focusing) {
+            if (fs.length == 5 && fs.indexOf(TimerState.LongBreaking) == -1) {
+                return TimerState.LongBreaking;
+            } else {
+                return TimerState.Breaking;
             }
-            updateFocusLsit(draft => { draft.push("focus") });
-            setState("stopped");
-        });
-        const intervalId = countdown.start(Number(setting.getItem("focus")));
-        setTimerId(() => { return intervalId });
-        setState("focusing");
-    };
-
-    const startBreakTimer = () => {
-        const countdown = new Countdown((timeDiff) => {
-            setValue(timeDiff);
-        }, () => {
-            const tip = showNotification("Break stopped");
-            tip.onclick = () => {
-                window.focus();
-            }
-            updateFocusLsit(draft => { draft.push("break") });
-            setState("stopped");
-        });
-        const intervalId = countdown.start(Number(setting.getItem("break")));
-        setTimerId(() => { return intervalId });
-        setState("breaking");
+        } else if (fs.at(-1) == TimerState.Breaking || fs.at(-1) == TimerState.LongBreaking) {
+            return TimerState.Focusing;
+        };
+        return TimerState.Focusing;
     };
 
 
-    const startLongBreakTimer = () => {
+    const startTimer = () => {
+        const timerType = selectTimerType();
         const countdown = new Countdown((timeDiff) => {
             setValue(timeDiff);
         }, () => {
-            const tip = showNotification("Break stopped");
+            const tip = showNotification(`${timerType} stopped`);
             tip.onclick = () => {
                 window.focus();
             }
-            updateFocusLsit(draft => { draft.push("longBreak") });
-            setState("stopped");
+            updateFocusLsit(draft => { draft.push(timerType) });
+            setState(TimerState.Stop);
         });
-        const intervalId = countdown.start(Number(setting.getItem("longBreak")));
+        const intervalId = countdown.start(Number(setting.getItem(timerType)));
         setTimerId(() => { return intervalId });
-        setState("Long breaking");
+        setState(timerType);
     };
+
 
     const extend = () => {
 
@@ -102,12 +94,12 @@ export default function Timer() {
             console.log("Not interval id");
         } else {
             clearInterval(timerId);
-            setState("");
+            setState(TimerState.Stop);
         }
     }
 
 
-    const format = (value: TimeProgress, state: string) => {
+    const format = (value: TimeProgress, state: TimerState) => {
         let hours = Math.floor(value.timeLeft / (1000 * 60 * 60));
         let minutes = Math.floor((value.timeLeft % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((value.timeLeft % (1000 * 60)) / 1000);
@@ -117,26 +109,9 @@ export default function Timer() {
         const formattedSeconds: string = seconds.toString().padStart(2, '0');
         return <>
             <p className="text-6xl font-semibold">{formattedHours}:{formattedMinutes}:{formattedSeconds}</p>
-            <p className="text-2xl text-gray-500">{state}</p>
+            {state == TimerState.Stop ? "" : <p className="text-2xl text-gray-500">{state}</p>}
         </>
     }
-
-    useEffect(() => {
-        const fs = focusList.slice(-5);
-        console.log(fs);
-        if (fs.at(-1) == "focus") {
-            if (fs.length == 5 && fs.indexOf("longBreak") == -1) {
-                startLongBreakTimer();
-            } else {
-                startBreakTimer();
-            }
-        } else if (fs.at(-1) == "break" || fs.at(-1) == "longBreak") {
-            startFocusTimer();
-        } else {
-            console.log("init");
-        }
-
-    }, [focusList])
 
     const config = {
         size: 400,
@@ -158,7 +133,7 @@ export default function Timer() {
                     <div className="justify-center items-center flex flex-col gap-4">
                         <TimerButton
                             state={state}
-                            startFunc={startFocusTimer}
+                            startFunc={startTimer}
                             stopFunc={stop}
                             extendFunc={extend}
                         />
